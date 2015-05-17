cd8a03e94ca5ecafad772bca4b67741d3cf68096
hgcummings <harry@xlr3.com>
2015-05-17 14:57:24 +0100

    Remove post-specific code

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ var fs = require('hexo-fs');
 fs.readdir(hexo.base_dir + '/source/_types').then(function(files) {
   files.forEach(function(item) {
     hexo.locals.set(item, function(){
-      var query = {};
-    
-      if (!hexo.config.future){
-        query.date = {$lte: Date.now()};
-      }
-    
-      return hexo.database.model(item).find(query);
+      return hexo.database.model(item);
     });
   });
 });
--- processor.js
+++ processor.js
@@ -3,40 +3,18 @@
 var common = require('./common');
 var Promise = require('bluebird');
 var yfm = require('hexo-front-matter');
-var pathFn = require('path');
-var fs = require('hexo-fs');
-var util = require('hexo-util');
-var slugize = util.slugize;
-var Pattern = util.Pattern;
-var Permalink = util.Permalink;
+var slugize = require('hexo-util').slugize;
+var Schema = require('warehouse').Schema;
 
 var permalink;
-
-var preservedKeys = {
-  title: true,
-  year: true,
-  month: true,
-  day: true,
-  i_month: true,
-  i_day: true
-};
-
-function startsWith(str, prefix){
-  return str.substring(0, prefix.length) === prefix;
-}
-
-exports.process = function(file){
-  return processPost.call(this, file);
-};
 
 exports.pattern = function(path){
   if (common.isTmpFile(path)) return false;
 
-  if (startsWith(path, '_types')){
+  if (path.indexOf('_types/') === 0){
     var relativePath = path.substr(path.indexOf('/') + 1);
     
     return {
-      path: relativePath,
       customType: relativePath.substr(0, relativePath.indexOf('/'))
     };
   }
@@ -44,13 +22,9 @@ exports.pattern = function(path){
   return false;
 };
 
-function processPost(file){
-  /* jshint validthis: true */
-  var Model = this.model(file.params.customType, this.model('Post').schema);
-  var path = file.params.path;
+exports.process = function(file){
+  var Model = this.model(file.params.customType, new Schema());
   var doc = Model.findOne({source: file.path});
-  var config = this.config;
-  var timezone = config.timezone;
 
   if (file.type === 'skip' && doc){
     return;
@@ -69,43 +43,14 @@ function processPost(file){
     file.read()
   ]).spread(function(stats, content){
     var data = yfm(content);
-    var info = parseFilename(config.new_post_name, path);
-    var keys = Object.keys(info);
-    var key;
-
     data.source = file.path;
     data.raw = content;
-    data.slug = info.title;
-    data.published = true;
-
-    for (var i = 0, len = keys.length; i < len; i++){
-      key = keys[i];
-      if (!preservedKeys[key]) data[key] = info[key];
-    }
-
-    if (data.date){
-      data.date = common.toDate(data.date);
-    } else if (info && info.year && (info.month || info.i_month) && (info.day || info.i_day)){
-      data.date = new Date(
-        info.year,
-        parseInt(info.month || info.i_month, 10) - 1,
-        parseInt(info.day || info.i_day, 10)
-      );
-    }
-
-    if (data.date){
-      if (timezone) data.date = common.timezone(data.date, timezone);
-    } else {
-      data.date = stats.ctime;
-    }
-
-    if (data.link && !data.title){
-      data.title = data.link.replace(/^https?:\/\/|\/$/g, '');
-    }
 
     if (data.permalink){
       data.slug = data.permalink;
       delete data.permalink;
+    } else {
+      data.slug = createSlug(file.path);
     }
 
     if (doc){
@@ -116,29 +61,9 @@ function processPost(file){
   });
 }
 
-function parseFilename(config, path){
-  config = config.substring(0, config.length - pathFn.extname(config).length);
-  path = path.substring(0, path.length - pathFn.extname(path).length);
-
-  if (!permalink || permalink.rule !== config){
-    permalink = new Permalink(config, {
-      segments: {
-        year: /(\d{4})/,
-        month: /(\d{2})/,
-        day: /(\d{2})/,
-        i_month: /(\d{1,2})/,
-        i_day: /(\d{1,2})/
-      }
-    });
-  }
-
-  var data = permalink.parse(path);
-
-  if (data){
-    return data;
-  } else {
-    return {
-      title: slugize(path)
-    };
-  }
+function createSlug(path) {
+  var parts = path.split('/');
+  var filename = parts[parts.length - 1];
+  var namePart = filename.substring(0, filename.lastIndexOf('.'));
+  return slugize(namePart);
 }
